ff485153f4439ed7771c41f0e5d22b421c4b55a1
Fluorouacil <artem.super.05@gmail.com>
2025-09-13 11:43:51 +0400

    Added comments

--- LED_scketch.ino
+++ LED_scketch.ino
@@ -1,20 +1,23 @@
-//#include "printf.h"
-
 const int GREEN = 3;
 const int YELLOW = 4;
 const int RED = 5;
 
 void setup() {
+  // Установим все пины области D на выход а пины области B на вход
   DDRD = 0b00000000;
   DDRB = 0b00111111;
   PORTD = 0b00000000;
+  // Запустим Serial для ведения логов
   Serial.begin(9600);
 }
 
 void loop() {
+  // Если есть какой-то сигнал в области D
   if (PIND != 0) {
+    // Считываем байт
       byte readed_byte = PIND;
 
+      // Если сигнал пришел из бита Green
       if ((readed_byte & (1 << GREEN)) != 0) {
       PINB = PINB | readed_byte;
       Serial.println("Кнопка нажата");
@@ -25,6 +28,7 @@ void loop() {
       Serial.print(" С байта ");
       Serial.print(readed_byte, BIN);
       Serial.print("\n");
+      // Если сигнал пришел из бита Yellow
     } if ((readed_byte & (1 << YELLOW)) != 0) {
       PINB = PINB | readed_byte;
       Serial.println("Кнопка нажата");
@@ -35,6 +39,7 @@ void loop() {
       Serial.print(" С байта ");
       Serial.print(readed_byte, BIN);
       Serial.print("\n");
+      // Если сигнал пришел из бита Red
     } if ((readed_byte & (1 << RED)) != 0) {
       PINB = PINB | readed_byte;
       Serial.println("Кнопка нажата");
